baef222168dab955e8624a47b51351a697df0729
Braulio-Manrique <brauliocuenta12@gmail.com>
2024-12-01 09:00:47 -0500

    primer commit

---
+++ src/test/java/pe/edu/cibertec/appbancocibertec/AppBancoCibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appbancocibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBancoCibertecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
