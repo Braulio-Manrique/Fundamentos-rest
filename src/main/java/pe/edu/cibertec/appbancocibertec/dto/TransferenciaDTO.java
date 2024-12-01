ff3a664edee4b0d3e622cbd38d2d8105ebb4e135
Braulio-Manrique <brauliocuenta12@gmail.com>
2024-12-01 10:42:53 -0500

    terminando la primera parte

--- src/main/java/pe/edu/cibertec/appbancocibertec/dto/TransferenciaDTO.java
+++ src/main/java/pe/edu/cibertec/appbancocibertec/dto/TransferenciaDTO.java
@@ -2,15 +2,13 @@ package pe.edu.cibertec.appbancocibertec.dto;
 
 import lombok.Data;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 
 @Data
 public class TransferenciaDTO {
     private Integer cuentaorigenid;
     private Integer cuentadestinoid;
     private Double monto;
-    private LocalDateTime fecha;
 
 
 }
