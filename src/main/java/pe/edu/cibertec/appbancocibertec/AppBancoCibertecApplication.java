fa5d4dc923701b1d021861779740a60d9627ba4d
Braulio-Manrique <brauliocuenta12@gmail.com>
2024-12-01 10:57:31 -0500

    cors

--- src/main/java/pe/edu/cibertec/appbancocibertec/AppBancoCibertecApplication.java
+++ src/main/java/pe/edu/cibertec/appbancocibertec/AppBancoCibertecApplication.java
@@ -2,6 +2,9 @@ package pe.edu.cibertec.appbancocibertec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AppBancoCibertecApplication {
@@ -10,4 +13,15 @@ public class AppBancoCibertecApplication {
         SpringApplication.run(AppBancoCibertecApplication.class, args);
     }
 
+
+
+    @Bean
+public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("api/v1/**").allowedMethods("GET", "POST", "PUT").allowedOrigins("http://frontbancocibertec.com");
+            }
+        };
+    }
 }
--- src/main/java/pe/edu/cibertec/appbancocibertec/controller/CuentaController.java
+++ src/main/java/pe/edu/cibertec/appbancocibertec/controller/CuentaController.java
@@ -4,17 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pe.edu.cibertec.appbancocibertec.dto.Cuentadto;
 import pe.edu.cibertec.appbancocibertec.dto.TransferenciaDTO;
 import pe.edu.cibertec.appbancocibertec.model.Transaccion;
 import pe.edu.cibertec.appbancocibertec.service.ICuentaService;
 
 import java.util.List;
-
+@CrossOrigin(origins = {"http:frontbancocber.com"}, methods = {RequestMethod.GET, RequestMethod.POST})
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/api/v1/cuenta")
